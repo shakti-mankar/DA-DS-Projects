7175a46f554054f14a9e46f708b03890dfd37c8a
Shakti-mankar <mailshaktimankar@gmail.com>
2026-08-07 10:00:00 +0530

    data cleaning complete

---
+++ End to End Project/Ecom_DB.sql
@@ -0,0 +1,59 @@
+Create database Ecommerce;
+
+use Ecommerce;
+
+select * from orders;
+
+-- Total Orders
+
+select count(*) as Total_orders from orders;
+
+-- Total Sales
+
+select sum(Net_Amount) as Total_Sales from orders;
+
+-- Top order
+
+Select product , sum(net_amount) as Sales
+from orders
+group by product
+order by sales desc;
+
+
+-- Top cities
+
+select City, sum(Net_amount) as Sales
+from orders
+Group by City
+order by Sales desc;
+
+-- Month wise sales
+
+select Month, Sum(Net_amount) as Sales
+from orders 
+group by Month
+order by sales desc;
+
+
+-- Highest profit product 
+ 
+select Product , sum(profit) as profit_sales
+from orders
+group by Product
+order by  profit_sales desc;
+
+
+--  distribution by payment type 
+
+select Payment_Mode , count(*) as Total_order
+from orders
+group by Payment_Mode;
+
+
+-- canceled orders
+
+select * from orders 
+where Order_Status = 'Cancelled';
+ 
+-- 
+ 
